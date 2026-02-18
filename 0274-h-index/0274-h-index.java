889b605313a5a615aa8dab1d0d102018dd74d364
ls843 <Laxman.G@bounteous.com>
2026-02-18 00:26:20 -0800

    Time: 5 ms (69%), Space: 43.6 MB (58.14%) - LeetHub

---
+++ 0274-h-index/0274-h-index.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+
+        for (int i = 0; i < citations.length / 2; i++) {
+            int temp = citations[i];
+            citations[i] = citations[citations.length - 1 - i];
+            citations[citations.length - 1 - i] = temp;
+        }
+
+        int hIndex = 0;
+        for(int i = 0; i < citations.length; i++) {
+
+            if(citations[i] > i) {
+                hIndex = i + 1;
+            }
+        }
+
+        return hIndex;
+    }
+}
